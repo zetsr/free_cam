b1429dcfbc3f5fa2d3ab8a8a70ce6cbeb02ff26d
zetsr <90092893+zetsr@users.noreply.github.com>
2025-03-17 22:30:55 +0800

    Delete free_cam directory

--- free_cam/pointer_scanner.h
+++
@@ -1,9 +0,0 @@
-#ifndef POINTER_SCANNER_H
-#define POINTER_SCANNER_H
-
-#include <windows.h>
-#include <vector>
-
-uintptr_t GetMultiLevelPointer(HANDLE hProcess, uintptr_t baseAddress, const std::vector<uintptr_t>& offsets);
-
-#endif
--- free_cam/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <string>
-
-std::wstring GetTimestamp();
-
-#endif
